a62e18aefb715e57ee248c20f9cf411996987372
Abhishk nigam <nigamabhishek140@gmail.com>
2024-06-11 01:48:47 +0530

    indclude operation

--- BASIC_DATA_STRUCTURE/ARRAY/Fourth.java
+++ BASIC_DATA_STRUCTURE/ARRAY/Fourth.java
@@ -1,5 +1,7 @@
 package BASIC_DATA_STRUCTURE.ARRAY;
 
+import java.util.Arrays;
+
 public class Fourth {
     public static void main(String[] args) {
         // -----------------------------------OPERATION ARRAY --------------------------------------------- 
@@ -8,6 +10,8 @@ public class Fourth {
         // -----------------------tranversal-------------------------
         // vist each element in array with its index through loops
         // and allocate the  memory for elements in system CAPACITY [4] 
+        // print all the array elements one by one.
+        
         // int [] arr = new int[4];
         // for (int i = 0; i < arr.length; i++) {
             
@@ -17,34 +21,108 @@ public class Fourth {
 
 
 
-         // -----------------------INSERTION-------------------------
+         // -----------------------INSERTION----------------------------------------------------
 
 
-        //  Arrays: In Java, arrays are fixed-size data structures that store elements of the same data type.
-        //   Check if the array has enough space to accommodate the new element. If not, you need to resize the array.
-        //  Find the index where you want to insert the new element.
-        //  Shift the elements from the insertion index to the end of the array by one position.
-        //  Insert the new element at the insertion index. 
+        // Arrays: In Java, arrays are fixed-size data structures that store elements of the same data type.
+        // Check if the array has enough space to accommodate the new element. If not, you need to resize the array.
+        // Find the index where you want to insert the new element.Shift the elements from the insertion index to the 
+        // end of the array by one position.Insert the new element at the insertion index. 
 
 
-        int[] arr = {1, 2, 3, 4, 5};
+        
+        // -----------------------------insert at last--------------------------------------
 
-        // Insert the element 6 at index 2
-        int index = 2;
-        int newElement = 6;
 
-        // Shift the elements from index 2 to the end of the array by one position
-        for (int i = arr.length - 1; i >= index; i--) {
-            arr[i + 1] = arr[i];
-        }
+    //       int[] originalArray = {1, 2, 3, 4, 5};
+    //    for (int i = 0; i < originalArray.length; i++) {
+            
+    //         System.out.println(originalArray[i]);
+    //     }
 
-        // Insert the new element at index 2
-        arr[index] = newElement;
 
-        // Print the updated array
-        for (int i = 0; i < arr.length; i++) {
-        System.out.println(arr[i]);
-    }
+    //     int newElement = 6;
+
+    //     // Create a new array with one additional slot
+    //     int[] newArray = new int[originalArray.length + 1];
+
+    //     // Copy elements from original array to new array
+    //     for (int i = 0; i < originalArray.length; i++) {
+    //         newArray[i] = originalArray[i];
+    //     }
+
+    //     // Insert the new element at the end
+    //     newArray[newArray.length - 1] = newElement;
+
+    //     // Print the new array
+    //     System.out.println("Array after insertion at the end: " + Arrays.toString(newArray));
+
+
+
+
+        // -----------------------------insert at specific point--------------------------------------
+
+
+
+        // int[] originalArray2 = {1, 2, 3, 4, 5};
+        // int newElement2 = 99;
+        // int position = 2; // Insert at index 2
+
+        // // Create a new array with one additional slot
+        // int[] newArray2 = new int[originalArray.length + 1];
+
+        // // Copy elements from original array to new array up to the insertion point
+        // for (int i = 0; i < position; i++) {
+        //     newArray2[i] = originalArray2[i];
+        // }
+
+        // // Insert the new element
+        // newArray2[position] = newElement2;
+
+        // // Copy the remaining elements from original array to new array
+        // for (int i = position; i < originalArray.length; i++) {
+        //     newArray2[i + 1] = originalArray2[i];
+        // }
+
+        // // Print the new array
+        // System.out.println("Array after insertion at position " + position + ": " + Arrays.toString(newArray2));
+
+
+        // -----------------------------------------UPDATED------------------------------------------
+        // int arr[] = new int[3];
+        // arr[0] = 1;
+        // arr[1] = 2;
+        // arr[2] = 3;
+        // for (int i = 0; i < arr.length; i++) {
+        //     System.out.print("ELEMENT BEFORE UPDATE " + arr[i]);
+        //     System.out.println();
+            
+        // }
+        // arr[0] = 4;
+        // arr[1] = 5;
+        // arr[2] = 6;
+        // System.out.println("ELEMENT AFTER UPDATE");
+        // for (int i = 0; i < arr.length; i++) {
+        //     System.out.print("ELEMENT AFTER UPDATE " + arr[i]);
+        //     System.out.println();
+        // }
+        // -----------------------------------------SEARCHING------------------------------------------
+
+        
+        // int [] arr = {1, 2, 3};
+        // int search = 3;
+        // for (int i = 0; i < arr.length; i++) {
+
+        //     if (arr[i] == search) {
+        //         System.out.println("ELEMENT FOUND AT INDEX " + i);
+        //         break;
+        //     }
+        // }
+
+
+    
+
+
 
 
 
--- BASIC_DATA_STRUCTURE/ARRAY/Third.java
+++ BASIC_DATA_STRUCTURE/ARRAY/Third.java
@@ -1,20 +1,20 @@
-package BASIC_DATA_STRUCTURE.ARRAY;
+// package BASIC_DATA_STRUCTURE.ARRAY;
 
-import java.util.Scanner;
+// import java.util.Scanner;
 
-public class Third {
-    public static void main(String[] args) {
-        System.out.println("ENTER THE SIZE OF ARRAY");
-        Scanner sc = new Scanner(System.in);
-        int size = sc.nextInt();
-        int [] arr = new int[size];
-        for (int i = 0 ; i < size ; i++) {
-            System.out.println("ENTER " + i + " ELEMENT ");
-             arr[i] = sc.nextInt();
+// public class Third {
+//     public static void main(String[] args) {
+//         System.out.println("ENTER THE SIZE OF ARRAY");
+//         Scanner sc = new Scanner(System.in);
+//         int size = sc.nextInt();
+//         int [] arr = new int[size];
+//         for (int i = 0 ; i < size ; i++) {
+//             System.out.println("ENTER " + i + " ELEMENT ");
+//              arr[i] = sc.nextInt();
 
 
-    }
-    for (int i = 0 ; i < size ; i++) {
-        System.out.println("ELEMENT " + i + ":" + arr[i] + " ");
+//     }
+//     for (int i = 0 ; i < size ; i++) {
+//         System.out.println("ELEMENT " + i + ":" + arr[i] + " ");
     
-}}}
+// }}}
